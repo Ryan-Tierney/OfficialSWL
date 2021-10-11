a8e24457e634a855e10124c543cabaad25cf29de
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-10-10 21:59:51 -0500

    added product to database

---
+++ backend/data/Products.js
@@ -0,0 +1,10 @@
+const Products = [
+  {
+    name: "Sorry We're Late T-Shirt",
+    ImageUrl: "N/A",
+    Description:
+      "men's shirt available in multiple sizes with the Sorry We're Late logo",
+    Price: 19.99,
+    CountInStock: 100
+  }
+];
