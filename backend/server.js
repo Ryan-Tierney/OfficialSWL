e0af1a9ec5b0be5a04160e86bf5c0c64d8e9dc0f
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-10-17 21:32:59 -0500

    adding routes

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const productRoutes = require("./routes/productRoutes");
 
 connectDB();
+
+app.use(express.json());
+
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
