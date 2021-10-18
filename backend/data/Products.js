7c10a9bab2ca85323e9b564765e1a57dba972c11
Ryan-Tierney <Ryan.Tierney92@gmail.com>
2021-10-17 21:07:04 -0500

    adding seederScript

--- backend/data/Products.js
+++ backend/data/Products.js
@@ -1,4 +1,4 @@
-const Products = [
+const products = [
   {
     name: "Sorry We're Late T-Shirt",
     ImageUrl: "N/A",
@@ -6,7 +6,7 @@ const Products = [
       "men's shirt available in multiple sizes with the Sorry We're Late logo",
     Price: 19.99,
     CountInStock: 100
-  }
+  },
 
   {
     name: "Sorry We're Late Vinyl Sticker",
@@ -14,8 +14,16 @@ const Products = [
     Description: "a durable sticker of the sorry we're late logo",
     Price: 3.99,
     CountinStock: 100
+  },
 
+  {
+    name: "Sorry We're Late Mug",
+    ImageUrl: "N/A",
+    Description:
+      "A 16 oz coffee mug emblazened with the Sorry We're Late Cast in cartoon form",
+    Price: 14.99,
+    CountinStock: 100
   }
-
-
 ];
+
+module.exports = products;
